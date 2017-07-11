b745ec70f30be41937201c0f091523ac87b9905d
Óscar Rodríguez Garrucho <orodriguez.garrucho@mvalle-xps-15-9550.atsistemas.lan>
2017-07-11 08:29:49 +0200

    Initial Commit

---
+++ ElitechLab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ElitechLab
+//
+//  Created by Óscar Rodríguez Garrucho on 11/7/17.
+//  Copyright © 2017 Óscar Rodríguez Garrucho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
